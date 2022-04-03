ef43e13e1d9e515de8c9d915f936c0848938385f
wajahat145 <44720835+wajahat145@users.noreply.github.com>
2022-04-03 15:32:20 +0500

    Add files via upload

---
+++ AesOperation_Form/AesOperation_Form/AesOperation_Form.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AesOperation_Form
+{
+    public partial class AesOperation_Form : Form
+    {
+        public AesOperation_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string plaintext = textBox1.Text.ToString();
+            if (string.IsNullOrEmpty(plaintext))
+            {
+                MessageBox.Show("plaintext required");
+            }
+            else
+            {
+               textBox3.Text = AesOperation.EncryptString(plaintext);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string encryptedText = textBox2.Text.ToString();
+            if (string.IsNullOrEmpty(encryptedText))
+            {
+                MessageBox.Show("encryptedtext required");
+            }
+            else
+            {
+                textBox3.Text = AesOperation.DecryptString(encryptedText);
+            }
+        }
+    }
+}
